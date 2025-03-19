d7c14986984ed1a27e95582fe3b94e89cd001ba9
jilong <jilongcui@163.com>
2025-03-19 16:30:30 +0800

    Add 南京大学 版权所有.

--- web/app/signin/layout.tsx
+++ web/app/signin/layout.tsx
@@ -31,7 +31,7 @@ export default async function SignInLayout({ children }: any) {
           </div>
         </div>
         <div className='px-8 py-6 system-xs-regular text-text-tertiary'>
-          © {new Date().getFullYear()} LangGenius, Inc. All rights reserved.
+          © {new Date().getFullYear()} 南京大学 版权所有
         </div>
       </div>
     </div>
